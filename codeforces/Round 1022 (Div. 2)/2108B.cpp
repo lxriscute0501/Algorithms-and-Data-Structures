da975e1f7601b7e93af43c82286cc690386da61c
十一十一 <lixuanran798@gmail.com>
2025-05-11 00:07:24 +0800

    upload

---
+++ codeforces/Round 1022 (Div. 2)/2108B.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n, x;
+        cin >> n >> x;
+        
+        if (n == 1) {
+            cout << (x != 0 ? x : -1) << '\n';
+            continue;
+        } else {
+            if (x == 0) {
+                if (n % 2 == 0) {
+                    cout << n << '\n';
+                    continue;
+                } else {
+                    cout << n + 3 << '\n';
+                    continue;
+                }
+            } else if (x == 1) {
+                if (n % 2 == 0) {
+                    cout << n + 3 << '\n';
+                    continue;
+                } else {
+                    cout << n << '\n';
+                    continue;
+                }
+            }
+        }
+
+        long long temp = x, cnt = 0;
+        while (temp > 0) {
+            cnt += temp % 2;
+            temp = temp >> 1;
+        }
+        
+        if (n >= cnt + 1) {
+            if ((n-cnt) % 2 == 0) cout << n-cnt+x << '\n';
+            else cout << n-cnt+x+1 << '\n';
+        } else cout << x << '\n';
+    }
+    return 0;
+}
---
+++ codeforces/Round 1022 (Div. 2)/2108C.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+ 
+using namespace std;
+ 
+int main()
+{
+    int tt = 1;
+    cin >> tt;
+    while(tt--) {
+        int n;
+        cin >> n;
+        vector<int> a;
+        a.push_back(-1e9);
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            if (a.back() != x) a.push_back(x);
+        }
+        a.push_back(-1e9);
+        
+        int ans = 0;
+        for (int i = 1; i < a.size() - 1; i++)
+            if (a[i - 1] < a[i] and a[i] > a[i + 1]) ans++;
+ 
+        cout << ans << endl;
+    }
+}
